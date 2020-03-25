ea8e8bd5d0a9090336e80b1c488321565118dee8
jensen-balavadivel-anbazhagan <59868712+jensen-balavadivel-anbazhagan@users.noreply.github.com>
2020-03-25 21:29:25 +0100

    Created the correct folder structure

---
+++ js/gallery.js
@@ -0,0 +1,17 @@
+  // javascript module for gallery (lightbox) functions 
+ function showPhoto(index, photos) {
+    document.getElementById('gallery').style.display = "block";
+    var container = document.getElementsByClassName('gallery__image')[0];
+    if (index >= 0 && index < photos.length) {
+       var photo = photos[index];
+       container.src = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+       '/' + photo.id + '_' + photo.secret + '_b.jpg';
+    }
+
+ }
+ 
+ function closeModal() {
+   document.getElementById('gallery').style.display = "none";
+  }
+  
+  export default {showPhoto, closeModal};
